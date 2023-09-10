7d6582118aa10cfe28f1aab77d4fb17a7d779696
MatiasS <fuxesosa@gmail.com>
2023-09-10 19:28:32 +0000

    add crud

--- src/app/component/categoria/categoria.component.ts
+++ src/app/component/categoria/categoria.component.ts
@@ -9,7 +9,13 @@ import { CategoriaService } from '../../service/categoria.service';
 })
 export class CategoriaComponent implements OnInit{
   categorias: Categoria[] = []
+  categoriaPrueba: Categoria = {_id:'', descripcion:''}
+  categoriaAdd: Categoria = {_id:'', descripcion:''}
+  categoriaDeleteId: string = ''
+  categoriaDeleted: Categoria = {_id:'', descripcion:''}
+
   constructor(private categoriaService: CategoriaService){}
+
   ngOnInit(): void {
      this.categoriaService.getAll().subscribe({
       next: (entity) => {
@@ -19,5 +25,35 @@ export class CategoriaComponent implements OnInit{
         console.log("No se pudieron conseguir las categorías:", error);
       }
     });
+    this.categoriaService.get("64f67e4cedfa4ac5bc60f631").subscribe({
+      next: (entity) => {
+        this.categoriaPrueba = entity;
+      },
+      error: (error) => {
+        console.log("No se pudieron conseguir las categorías:", error);
+      }
+    });
+
+  }
+  createCategoria(): void{
+    this.categoriaService.create(this.categoriaAdd).subscribe({
+      next: (entity) => {
+        this.categoriaAdd = entity;
+      },
+      error: (error) => {
+        console.log("No se pudieron conseguir las categorías:", error);
+      }
+    });
+  }
+  deleteCategoria(): void{
+    this.categoriaService.delete(this.categoriaDeleteId).subscribe({
+      next: (entity) => {
+        this.categoriaDeleted = entity;
+      },
+      error: (error) => {
+        console.log("No se pudieron conseguir las categorías:", error);
+      }
+    });
+
   }
 }
